f555d9181277dc58aa258b3255ea43ec0fcfeb0e
Rajat Kumar Gupta <rajatcube@gmail.com>
2018-07-10 14:41:18 +0530

    Initialize and setup recycler view

--- app/src/main/java/com/example/knightcube/recipeapp/ui/MainActivity.java
+++ app/src/main/java/com/example/knightcube/recipeapp/ui/MainActivity.java
@@ -48,91 +48,95 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.Lis
         setSupportActionBar(toolbar);
 
         //Initialize Library
-        AndroidNetworking.initialize(getApplicationContext());
-
-        getAllRecipeFromAPI();
+        //TODO - 2 Uncomment to initialize library
+//        AndroidNetworking.initialize(getApplicationContext());
+//        getAllRecipeFromAPI();
 
     }
 
-
+    //TODO - 3 Uncomment to start json parsing
     //Fetch Data using custom parser
-    public List<Recipe> getAllRecipeFromAPI() {
-        final List<Recipe> recipeList = new ArrayList<>();
-        ingredientList = new ArrayList<Ingredient>();
-        stepList = new ArrayList<>();
-        AndroidNetworking.get(Constants.API_URL)
-                .setPriority(Priority.MEDIUM)
-                .build()
-                .getAsJSONArray(new JSONArrayRequestListener() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        if (response.toString() != null) {
-                            try {
-                                JSONArray jsonArray = new JSONArray(response.toString());
-                                for (int i = 0; i < jsonArray.length(); i++) {
-                                    JSONObject recipeJSONObject = jsonArray.getJSONObject(i);
-                                    String recipeName = recipeJSONObject.getString("name");
-                                    int id = recipeJSONObject.getInt("id");
-                                    int servings = recipeJSONObject.getInt("servings");
-                                    String image = recipeJSONObject.getString("image");
+//    public List<Recipe> getAllRecipeFromAPI() {
+//        final List<Recipe> recipeList = new ArrayList<>();
+//        ingredientList = new ArrayList<Ingredient>();
+//        stepList = new ArrayList<>();
+//        AndroidNetworking.get(Constants.API_URL)
+//                .setPriority(Priority.MEDIUM)
+//                .build()
+//                .getAsJSONArray(new JSONArrayRequestListener() {
+//                    @Override
+//                    public void onResponse(JSONArray response) {
+//                        if (response.toString() != null) {
+//                            try {
+//                                JSONArray jsonArray = new JSONArray(response.toString());
+//                                for (int i = 0; i < jsonArray.length(); i++) {
+//                                    JSONObject recipeJSONObject = jsonArray.getJSONObject(i);
+//                                    String recipeName = recipeJSONObject.getString("name");
+//                                    int id = recipeJSONObject.getInt("id");
+//                                    int servings = recipeJSONObject.getInt("servings");
+//                                    String image = recipeJSONObject.getString("image");
+//
+//
+//                                    //Getting ingredients of every recipe
+//                                    JSONArray ingredientsJSONArray = recipeJSONObject.getJSONArray("ingredients");
+//                                    for (int j = 0; j < ingredientsJSONArray.length(); j++) {
+//                                        JSONObject ingredientJsonObject = ingredientsJSONArray.getJSONObject(j);
+//                                        double quantity = ingredientJsonObject.getDouble("quantity");
+//                                        String measure = ingredientJsonObject.getString("measure");
+//                                        String ingredientString = ingredientJsonObject.getString("ingredient");
+//                                        Ingredient ingredient = new Ingredient(quantity, measure, ingredientString);
+//                                        ingredientList.add(ingredient);
+//                                    }
+//
+//                                    //Getting steps of every recipe
+//                                    JSONArray stepsJSONArray = recipeJSONObject.getJSONArray("steps");
+//                                    for (int k = 0; k < stepsJSONArray.length(); k++) {
+//                                        JSONObject stepsJsonObject = stepsJSONArray.getJSONObject(k);
+//                                        int stepId = stepsJsonObject.getInt("id");
+//                                        String shortDescription = stepsJsonObject.getString("shortDescription");
+//                                        String description = stepsJsonObject.getString("description");
+//                                        String videoURL = stepsJsonObject.getString("videoURL");
+//
+//                                        Step step = new Step(stepId, shortDescription, description, videoURL);
+//
+//                                        stepList.add(step);
+//                                    }
+//
+//                                    Recipe recipe = new Recipe(id, recipeName, ingredientList, stepList, servings, image);
+//                                    recipeList.add(recipe);
+//                                    Log.i(TAG, "onResponse: " + recipeList.size());
+//                                    setupAdapter(recipeList);
+//
+//                                }
+//                            } catch (JSONException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onError(ANError anError) {
+//                        Log.i(TAG, "onError: " + anError.getErrorBody());
+//                    }
+//                });
+//        return recipeList;
+//    }
 
-
-                                    //Getting ingredients of every recipe
-                                    JSONArray ingredientsJSONArray = recipeJSONObject.getJSONArray("ingredients");
-                                    for (int j = 0; j < ingredientsJSONArray.length(); j++) {
-                                        JSONObject ingredientJsonObject = ingredientsJSONArray.getJSONObject(j);
-                                        double quantity = ingredientJsonObject.getDouble("quantity");
-                                        String measure = ingredientJsonObject.getString("measure");
-                                        String ingredientString = ingredientJsonObject.getString("ingredient");
-                                        Ingredient ingredient = new Ingredient(quantity, measure, ingredientString);
-                                        ingredientList.add(ingredient);
-                                    }
-
-                                    //Getting steps of every recipe
-                                    JSONArray stepsJSONArray = recipeJSONObject.getJSONArray("steps");
-                                    for (int k = 0; k < stepsJSONArray.length(); k++) {
-                                        JSONObject stepsJsonObject = stepsJSONArray.getJSONObject(k);
-                                        int stepId = stepsJsonObject.getInt("id");
-                                        String shortDescription = stepsJsonObject.getString("shortDescription");
-                                        String description = stepsJsonObject.getString("description");
-                                        String videoURL = stepsJsonObject.getString("videoURL");
-
-                                        Step step = new Step(stepId, shortDescription, description, videoURL);
-
-                                        stepList.add(step);
-                                    }
-
-                                    Recipe recipe = new Recipe(id, recipeName, ingredientList, stepList, servings, image);
-                                    recipeList.add(recipe);
-                                    Log.i(TAG, "onResponse: " + recipeList.size());
-                                    setupAdapter(recipeList);
-
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onError(ANError anError) {
-                        Log.i(TAG, "onError: " + anError.getErrorBody());
-                    }
-                });
-        return recipeList;
-    }
 
     private void setupAdapter(List<Recipe> recipeList) {
         Log.i(TAG, "setupAdapter: " + recipeList.size());
         //Setup an adapter
-        recipeAdapter = new RecipeAdapter(recipeList, MainActivity.this, MainActivity.this);
+        //TODO - 3 Uncomment to setup adapter
+//        recipeAdapter = new RecipeAdapter(recipeList, MainActivity.this, MainActivity.this);
 
         //Setup RecyclerView
-        recyclerView = findViewById(R.id.recipe_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
+        //TODO - 4 Uncomment to setup RecyclerView
+//        recyclerView = findViewById(R.id.recipe_recycler_view);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
 
         //Set Adapter inside RecyclerView
-        recyclerView.setAdapter(recipeAdapter);
+        //TODO - 5 Uncomment to set adapter inside RecyclerView
+//        recyclerView.setAdapter(recipeAdapter);
     }
 
     @Override
